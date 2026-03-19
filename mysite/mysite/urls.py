498cca57530a82a25388368a3d085df27e359879
AaronD-code <b00164709@mytudublin.ie>
2026-03-19 17:37:46 +0000

    Add homepage redirect to polls

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.shortcuts import redirect
+
+def home(request):
+    return redirect('/polls/')
 
 urlpatterns = [
-    path("polls/", include("polls.urls")),
-    path("admin/", admin.site.urls),
+    path('', home),
+    path('polls/', include('polls.urls')),
+    path('admin/', admin.site.urls),
 ]
